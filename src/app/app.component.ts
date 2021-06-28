0e3e7a28bf3f13f00709b4a5baed6efc50ba79cd
Mickael Lherminez <mickael.Lherminez@gmail.com>
2021-06-29 00:24:40 +0200

    What is a component?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { Character } from './character';
   
 @Component({
   selector: 'bigbang-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  
+  name = 'Angular'; 
+
+  private characters: Character[];
+}
---
+++ src/app/character.ts
@@ -0,0 +1,7 @@
+export class Character {
+  id: number;
+  nbrSeason: number;
+  name: string;
+  picture: string;
+  season: Array<number>;
+}
