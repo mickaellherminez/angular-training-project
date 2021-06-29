8b9bcb04e3c894ba9e38a262cae0116798b9d398
Mickael Lherminez <mickael.Lherminez@gmail.com>
2021-06-29 07:01:46 +0200

    Add Characters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
 import { Character } from './character';
+import { CHARACTERS } from './mock-characters'
   
 @Component({
   selector: 'bigbang-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { 
+export class AppComponent implements OnInit { 
   
   name = 'Angular'; 
 
   private characters: Character[];
+
+  ngOnInit() {
+    this.characters = CHARACTERS;
+  }
 }
---
+++ src/app/mock-characters.ts
@@ -0,0 +1,88 @@
+import { Character } from './character';
+  
+export const CHARACTERS: Character[] = [
+    {
+        id: 1,
+        name: "Sheldon Cooper",
+        nbrSeason: 12,
+        picture: "https://fr.wikipedia.org/wiki/Jim_Parsons#/media/Fichier:Jim_Parsons_at_PaleyFest_2013.jpg",
+        season: [1,2,3,4,5,6,7,8,9,10,11,12],
+    },
+    {
+        id: 2,
+        name: "Leonard Hofstadter",
+        nbrSeason: 12,
+        picture: "https://fr.wikipedia.org/wiki/Leonard_Hofstadter#/media/Fichier:Johnny_Galecki_at_PaleyFest_2013.jpg",
+        season: [1,2,3,4,5,6,7,8,9,10,11,12],
+    },
+    {
+        id: 3,
+        name: "Penny",
+        nbrSeason: 12,
+        picture: "https://fr.wikipedia.org/wiki/The_Big_Bang_Theory#/media/Fichier:Kaley_Cuoco_by_Gage_Skidmore.jpg",
+        season: [1,2,3,4,5,6,7,8,9,10,11,12],
+    },
+    {
+        id: 4,
+        name: "Howard Wolowitz",
+        nbrSeason: 12,
+        picture: "https://fr.wikipedia.org/wiki/The_Big_Bang_Theory#/media/Fichier:Simon_Helberg_at_PaleyFest_2013.jpg",
+        season: [1,2,3,4,5,6,7,8,9,10,11,12],
+    },
+    {
+        id: 5,
+        name: "Raj Koothrappali",
+        nbrSeason: 12,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/016.png",
+        season: [1,2,3,4,5,6,7,8,9,10,11,12],
+    },
+    {
+        id: 6,
+        name: "Bernadette Maryann Rostenkowski",
+        nbrSeason: 8,
+        picture: "https://fr.wikipedia.org/wiki/The_Big_Bang_Theory#/media/Fichier:Melissa_Rauch_at_PaleyFest_2013_(edited).jpg",
+        season: [4,5,6,7,8,9,10,11,12],
+    },
+    {
+        id: 7,
+        name: "Amy Farrah Fowler",
+        nbrSeason: 8,
+        picture: "https://fr.wikipedia.org/wiki/The_Big_Bang_Theory#/media/Fichier:Mayim_Bialik_at_PaleyFest_2013.jpg",
+        season: [4,5,6,7,8,9,10,11,12],
+    },
+    {
+        id: 8,
+        name: "Stuart Bloom",
+        nbrSeason: 4,
+        picture: "https://fr.wikipedia.org/wiki/Kevin_Sussman#/media/Fichier:Kevin_Sussman_by_Gage_Skidmore.jpg",
+        season: [1,2],
+    },
+    {
+        id: 9,
+        name: "Mary Cooper",
+        nbrSeason: 4,
+        picture: "https://static.wikia.nocookie.net/bigbangtheory/images/5/57/Aws82.png/revision/latest/scale-to-width-down/666?cb=20180515232237",
+        season: [4,5],
+    },
+    {
+        id: 10,
+        name: "Beverly Hofstadter",
+        nbrSeason: 2,
+        picture: "https://fr.wikipedia.org/wiki/Christine_Baranski#/media/Fichier:Christine_Baranski_at_2015_PaleyFest.jpg",
+        season: [1,2],
+    },
+    {
+        id: 11,
+        name: "Barry Kripke",
+        nbrSeason: 2,
+        picture: "https://fr.wikipedia.org/wiki/John_Ross_Bowie#/media/Fichier:John_Ross_Bowie_by_Gage_Skidmore.jpg",
+        season: [1,2],
+    },
+    {
+        id: 12,
+        name: "Leslie Winkle",
+        nbrSeason: 2,
+        picture: "https://fr.wikipedia.org/wiki/Sara_Gilbert#/media/Fichier:Sara_Gilbert.jpg",
+        season: [1,2],
+    }
+];
